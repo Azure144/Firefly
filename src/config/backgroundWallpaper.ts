44dae7e6cc65eb04a0701a67ad89fff725bdbb2d
丰川祥子 <18720296292@163.com>
2026-08-22 21:40:01 +0800

    更新音乐API实现在线听歌，删除了一些图片

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -59,7 +59,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
 		// 支持远程视频URL，本地视频请放在 public/assets/videos/ 目录下
 		// playerUrl: "/assets/videos/firefly.mp4",
-		playerUrl: "https://bed.twoleaf.cn/file/1785658612716_firefly.mp4",
+		playerUrl: "https://img.coccineus.uk/file/1787400253967_DECO＊27_-_DECO＊27_-_テレパシ_feat._初音ミク.webm",
 	},
 	// 横幅壁纸和全屏壁纸共享配置
 	common: {
--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -26,7 +26,7 @@ export const sponsorConfig: SponsorConfig = {
 			name: "支付宝",
 			icon: "fa7-brands:alipay",
 			// 收款码图片路径（需要放在 public 目录下）
-			qrCode: "/assets/images/sponsor/alipay.png",
+			qrCode: "/assets/images/sponsor/alipay.jpg",
 			link: "",
 			description: "使用 支付宝 扫码打赏",
 			enabled: true,
@@ -34,7 +34,7 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "微信",
 			icon: "fa7-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png",
+			qrCode: "/assets/images/sponsor/wechat.jpg",
 			link: "",
 			description: "使用 微信 扫码打赏",
 			enabled: true,
@@ -44,8 +44,8 @@ export const sponsorConfig: SponsorConfig = {
 			icon: "simple-icons:kofi",
 			qrCode: "",
 			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
+			description: "Buy a Coffee for Coccineus",
+			enabled: false,
 		},
 		{
 			name: "爱发电",
@@ -53,7 +53,7 @@ export const sponsorConfig: SponsorConfig = {
 			qrCode: "",
 			link: "https://ifdian.net/a/cuteleaf",
 			description: "通过 爱发电 进行打赏",
-			enabled: true,
+			enabled: false,
 		},
 	],
 
@@ -74,6 +74,13 @@ export const sponsorConfig: SponsorConfig = {
 			// avatar: "",
 			amount: "¥20",
 			date: "2025-10-01",
+			
+		},
+		{
+			name: "特朗普",
+			// avatar: "",
+			amount: "¥100",
+			date: "2026-8-20",
 		},
 	],
 };
